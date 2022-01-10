32fd3336e459ac006a21ed6704d1b8449d827e9c
gkoh1 <75505740+gkoh1@users.noreply.github.com>
2022-01-10 16:16:47 -0600

    Added opmode for learning splines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -70,7 +70,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 87;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_VEL = Math.toRadians(270);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
 

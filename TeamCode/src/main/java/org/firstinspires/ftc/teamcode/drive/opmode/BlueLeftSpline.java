36280827ac184c9f5e7c3e8b1c73c898fd044018
gkoh1 <75505740+gkoh1@users.noreply.github.com>
2022-01-12 17:38:28 -0600

    Added to Blue path. It remains inconsistent but can get in and out of the walled-off area.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BlueLeftSpline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BlueLeftSpline.java
@@ -15,20 +15,28 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Autonomous
-public class SampleSpline extends LinearOpMode {
+public class BlueLeftSpline extends LinearOpMode {
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d startPose = new Pose2d(10, 72, Math.toRadians(270));
+        // It takes a really long time before this runs for some reason
+        Pose2d startPose = new Pose2d(6, 66, Math.toRadians(270));
         drive.setPoseEstimate(startPose);
         Trajectory myTrajectory1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(-9, 48), Math.toRadians(270))
+                .splineTo(new Vector2d(-12, 40), Math.toRadians(270))
                 .build();
         Trajectory myTrajectory2 = drive.trajectoryBuilder(myTrajectory1.end())
                 //Improve this to make it less choppy
-                .splineTo(new Vector2d(-9, 60), Math.toRadians(0))
-                .splineTo(new Vector2d(10, 76), Math.toRadians(0))
-                .forward(48)
+                .splineTo(new Vector2d(-8, 55), Math.toRadians(45))
+                .splineTo(new Vector2d(0, 68), Math.toRadians(10))
+                //.splineTo(new Vector2d(10, 76), Math.toRadians(0))
+                .splineTo(new Vector2d(48, 68), Math.toRadians(0))
+                .build();
+        Trajectory myTrajectory3 = drive.trajectoryBuilder(myTrajectory2.end())
+                .splineTo(new Vector2d(44, 54), Math.toRadians(90))
+                .splineTo(new Vector2d(40, 68.5), Math.toRadians(175))
+                .splineTo(new Vector2d(12, 72), Math.toRadians(180))
+                .splineTo(new Vector2d(-12, 40), Math.toRadians(270))
                 .build();
 
 
@@ -37,6 +45,10 @@ public class SampleSpline extends LinearOpMode {
         if(isStopRequested()) return;
 
         drive.followTrajectory(myTrajectory1);
+        sleep(1000);
+        drive.followTrajectory(myTrajectory2);
+        sleep(1000);
+        drive.followTrajectory(myTrajectory3);
         sleep(1000);
         drive.followTrajectory(myTrajectory2);
 

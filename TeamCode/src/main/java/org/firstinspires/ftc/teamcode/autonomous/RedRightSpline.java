d9d30a5b41734c428bf68a252cfc8ab6de3e77de
gkoh1 <75505740+gkoh1@users.noreply.github.com>
2022-02-10 17:13:20 -0600

    Added functions for CV to autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedRightSpline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedRightSpline.java
@@ -16,13 +16,29 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.subsystems.LoopyPipeline2;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous
 public class RedRightSpline extends LinearOpMode {
+
+    OpenCvCamera webcam;
+    LoopyPipeline2 pipeline;
+
+    CupPosition cupPos;
+
+    public enum CupPosition {
+        LEFT, // A
+        MIDDLE, // B
+        RIGHT // C
+    }
     @Override
 
     public void runOpMode() {
@@ -97,6 +113,58 @@ public class RedRightSpline extends LinearOpMode {
         drive.followTrajectorySequence(newTraj);
 
 
+
+    }
+
+    private void initCV() {
+        // Sets variable for the camera id
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        // Gives a name to the webcam
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        // Combines the above to create a webcam that we will use
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        //Sets our pipeline to view images through as the one we want
+        //(Boundary between regions 1 and 2, Boundary between 2 and 3, Far left, Far top, Far right, Far bottom, opmode, the side we're on)
+        pipeline = new LoopyPipeline2(80, 165, 40, 130, 245, 140, this);
+        webcam.setPipeline(pipeline);
+
+        // Turns on the webcam
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+            //This is needed so it knows what to do if something goes wrong
+            public void onError(int thing){
+                telemetry.addData("error",thing );
+            }
+
+        });
+
+    }
+
+    private void findCup() {
+
+//Takes some time so we can run everything through the pipeline
+        sleep(3000);
+
+        LoopyPipeline2.Position cupPosition = pipeline.position;
+
+//Sets cupPos to a corresponding position basesd on pipeline analysis
+
+        switch (cupPosition) {
+            case LEFT:
+                cupPos = CupPosition.LEFT;
+                break;
+            case MIDDLE:
+                cupPos = CupPosition.MIDDLE;
+                break;
+            case RIGHT:
+                cupPos = CupPosition.RIGHT;
+                break;
+        }
 
     }
 }
